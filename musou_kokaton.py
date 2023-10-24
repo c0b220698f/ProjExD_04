cf4195a6d2b4c33aa7f68152f6619d4c600fe95a
taku <c0b22061a5@edu.teu.ac.jp>
2023-10-24 16:00:35 +0900

    1

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -72,6 +72,7 @@ class Bird(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = xy
         self.speed = 10
+        self_LSHIFT_press = False
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -88,6 +89,14 @@ class Bird(pg.sprite.Sprite):
         引数1 key_lst：押下キーの真理値リスト
         引数2 screen：画面Surface
         """
+        if key_lst[pg.K_LSHIFT]:
+            self_LSHIFT_press = True
+        else:
+            self_LSHIFT_press = False
+        if self_LSHIFT_press:
+            self.speed = 20
+        else:
+            self.speed = 10
         sum_mv = [0, 0]
         for k, mv in __class__.delta.items():
             if key_lst[k]:
@@ -283,8 +292,6 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return 0
-            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                beams.add(Beam(bird))
         screen.blit(bg_img, [0, 0])
 
         if tmr % 200 == 0:  # 200フレームに1回，敵機を出現させる
